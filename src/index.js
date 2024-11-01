0c45cc64d419111f171a700931d3b39f3d6eed3a
askribilel123 <askribilel09@gmail.com>
2024-11-01 08:22:52 +0100

    build multiStage docker image using docker args passed by docker compose

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello Docker from container lesson 11</h1>");
+  res
+    .status(200)
+    .send("<h1>Hello Docker from container environment variable</h1>");
 });
 
 app.listen(PORT, () => {
