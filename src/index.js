2c6b0729f626b177f3e65a56f057abc4ab34f14f
askribilel123 <askribilel09@gmail.com>
2024-11-03 07:58:59 +0100

    connect node app to mongo container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
+const app = express();
+
+// connect to db
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((error) => {
+    console.error("failed to connect to db: ", error);
+  });
 
 app.get("/", (req, res) => {
   res
